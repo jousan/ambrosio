65ec2a6cf0a4de6cb3ee614e06668dda9c069703
g@g.com <g@g.com>
2016-04-19 15:49:27 +0200

    updates

--- ambrosio/ambrosio.py
+++ ambrosio/ambrosio.py
@@ -1,24 +1,72 @@
-#!/usr/bin/env_python
-#: -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from commandlist import CommandList
+import channels as ch #ch is alias of channels
+import actions as ac
+import time
 
 class Ambrosio(object):
-    """class for Ambrosio Personal Digital Butler
-
-    will run our house"""
+    """class for Ambrosio Personal Digital Butler will run our house"""
     def __init__(self):
             super(Ambrosio, self).__init__()
+            self.cl = CommandList()
+            self.channels = []
+            self.channels.append(ch.TextChannel())
+            self.channels.append(ch.TelegramChannel())
 
+            self.actions = []
+            self.actions.append(ac.MusicPlayer())
+
+
+    def next_command(self):
+        try:
+            return self.cl.next()
+        except:
+            return None,None
+
+    def update_channels(self):
+        for chan in self.channels:
+            while chan.msg_avail():
+                self.cl.append((chan, chan.get_msg()))
+                 #canal i missage es una tuple
+
+    def execute_command(self, command):
+        print "Will execute", command
+        # for each action in actions
+        # if is_for_you()
+        # action.do
+        words = command.split()
+        first_words = words[0]
+        rest_words = words[1:] #from position 1 until the end
+        response = None
+        for a in self.actions:
+            if a.is_for_you(first_words):
+                response = a.do(rest_words)
+                break
+        else:  #if there is no word after finishing the bucle, is the else form for
+            print "No t'entenc"
+        return response
 
     def mainloop(self):
-        #While True:
+        # While True:
         # command = get_command
         # do_command(command)
         # update
-        pass
+        while True:
+            chan, command = self.next_command()
+            if command:
+                #print command
+                response = self.execute_command(command)
+                chan.respond(response)
+
+            time.sleep(1)
+            self.update_channels()
 
 
 
 
 if __name__ == "__main__":
     print "Here be dragons!"
+    amb=Ambrosio()
+    amb.mainloop()
--- ambrosio/commandlist.py
+++ ambrosio/commandlist.py
@@ -1,12 +1,12 @@
 
 
 class CommandList(object):
-    def __init_(self):
+    def __init__(self):
         super(CommandList, self).__init__()
         self.commands = []
 
     def next(self): #next is the same that get_command
-        return self.commands.pop(0) #cua es afegir pel final i desplaçar pel principi FIFO
+        return self.commands.pop(0) #cua es afegir pel final i desplasar pel principi FIFO
 
     def append(self, command):
         self.commands.append(command)
